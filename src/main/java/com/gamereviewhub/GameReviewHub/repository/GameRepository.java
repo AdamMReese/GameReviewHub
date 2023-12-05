87f14a23ac676d2355612ce2564349f90f83007d
Adam Reese <64161641+AdamMReese@users.noreply.github.com>
2023-12-05 17:29:04 -0600

    Delete src/main/java/com/gamereviewhub/GameReviewHub directory

--- src/main/java/com/gamereviewhub/GameReviewHub/repository/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gamereviewhub.gamereviewhub.repository;
-
-import com.gamereviewhub.gamereviewhub.model.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
